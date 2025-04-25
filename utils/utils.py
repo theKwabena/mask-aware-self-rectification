834f873a016a8713f9be273a6cb0a6982254a156
et.abbey <abbeyemmanueltetteh@gmail.com>
2025-04-25 04:30:34 +0000

    added - readme

--- utils/utils.py
+++ utils/utils.py
@@ -1,16 +1,24 @@
+import os
+
 from torchvision.io import read_image
 import torch.nn.functional as F
 
 class Utils:
-    def load_image(image_path, res, device):
+    # ---------------------------------------------------
+    @staticmethod
+    def load_image(image_path:str, res, device):
         image = read_image(image_path)
         image = image[:3].unsqueeze_(0).float() / 127.5 - 1.
         image = F.interpolate(image, (res, res))
         return image.to(device)
 
-    def load_mask(mask_path, res, device):
+    # ---------------------------------------------------
+    @staticmethod
+    def load_mask(mask_path:str, res, device):
+        if not os.path.exists(mask_path):
+            return None
         mask = read_image(mask_path).float() / 255.
         if mask.shape[0] > 1:
-            mask = mask.mean(dim=0, keepdim=True)  # convert to grayscale
+            mask = mask.mean(dim=0, keepdim=True)
         mask = F.interpolate(mask.unsqueeze(0), (res, res))
         return mask.to(device)
